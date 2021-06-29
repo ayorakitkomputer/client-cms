dd3b2e0c5d7e49081833640b56105a5ab15584de
rantoinette <raissa.antoinette@gmail.com>
2021-06-29 23:37:12 +0700

    added logo

--- src/components/Topbar.vue
+++ src/components/Topbar.vue
@@ -1,7 +1,13 @@
 <template>
   <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
     <!-- Navbar Brand-->
-    <router-link class="navbar-brand ps-3" to="/">ARK</router-link>
+    <router-link class="navbar-brand ps-3" to="/"
+      ><img
+        src="https://cdn.discordapp.com/attachments/857558201851445250/859364209418043392/LOGO_TES.png"
+        style="width: 100px; height: 50px"
+        alt="Italian Trulli"
+      />
+    </router-link>
     <!-- Sidebar Toggle-->
     <button
       class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0"
@@ -10,29 +16,7 @@
     >
       <i class="fas fa-bars"></i>
     </button>
-    <!-- Navbar Search-->
-    <form
-      class="
-        d-none d-md-inline-block
-        form-inline
-        ms-auto
-        me-0 me-md-3
-        my-2 my-md-0
-      "
-    >
-      <div class="input-group">
-        <input
-          class="form-control"
-          type="text"
-          placeholder="Search for..."
-          aria-label="Search for..."
-          aria-describedby="btnNavbarSearch"
-        />
-        <button class="btn btn-primary" id="btnNavbarSearch" type="button">
-          <i class="fas fa-search"></i>
-        </button>
-      </div>
-    </form>
+
     <!-- Navbar-->
     <ul class="navbar-nav ms-auto ms-md-0 me-3 me-lg-4">
       <li class="nav-item dropdown">
