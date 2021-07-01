9482f909096cb82bb179bac77ed6c841aae0395a
Jethro Pierce <jethro.kapantow@gmail.com>
2021-07-01 20:26:24 +0700

    Change Server

--- src/views/Products.vue
+++ src/views/Products.vue
@@ -1,48 +1,48 @@
 <template>
-  <div class="container-fluid">
-    <!-- Page Heading -->
+	<div class="container-fluid">
+		<!-- Page Heading -->
 
-    <!-- DataTales Example -->
-    <div class="card shadow mb-4">
-      <div class="card-body">
-        <h1 class="h3 font-weight-bold mb-2 text-gray-800">
-          {{ this.$route.name }}
-        </h1>
-        <zing-grid
-          editor-controls
-          pager
-          filter
-          search
-          sort
-          zebra
-          gridlines="both"
-          page-size="10"
-          page-size-card="10"
-          page-size-options="2,4,10,20"
-          layout="row"
-          role="grid"
-          editor="inline"
-          viewport="tablet-portrait"
-          theme="ios"
-        >
-          <zg-data :src="dataUrl">
-            <zg-param name="recordPath" value="data"></zg-param>
-            <zg-param name="idKey" value="_id"></zg-param>
-            <zg-param name="headers" :value="access_token"></zg-param>
-            <StorageTable v-if="this.$route.path === '/storages'" />
-            <CpuTable v-if="this.$route.path === '/cpu'" />
-            <PowerSupplyTable v-if="this.$route.path === '/power_supplies'" />
-            <MonitorTable v-if="this.$route.path === '/monitors'" />
-            <GpuTable v-if="this.$route.path === '/gpu'" />
-            <MotherboardTable v-if="this.$route.path === '/motherboard'" />
-            <CaseTable v-if="this.$route.path === '/case'" />
-            <MemoryTable v-if="this.$route.path === '/memory'" />
-            <CaseFanTable v-if="this.$route.path === '/case_fan'" />
-          </zg-data>
-        </zing-grid>
-      </div>
-    </div>
-  </div>
+		<!-- DataTales Example -->
+		<div class="card shadow mb-4">
+			<div class="card-body">
+				<h1 class="h3 font-weight-bold mb-2 text-gray-800">
+					{{ this.$route.name }}
+				</h1>
+				<zing-grid
+					editor-controls
+					pager
+					filter
+					search
+					sort
+					zebra
+					gridlines="both"
+					page-size="10"
+					page-size-card="10"
+					page-size-options="2,4,10,20"
+					layout="row"
+					role="grid"
+					editor="inline"
+					viewport="tablet-portrait"
+					theme="ios"
+				>
+					<zg-data :src="dataUrl">
+						<zg-param name="recordPath" value="data"></zg-param>
+						<zg-param name="idKey" value="_id"></zg-param>
+						<zg-param name="headers" :value="access_token"></zg-param>
+						<StorageTable v-if="this.$route.path === '/storages'" />
+						<CpuTable v-if="this.$route.path === '/cpu'" />
+						<PowerSupplyTable v-if="this.$route.path === '/power_supplies'" />
+						<MonitorTable v-if="this.$route.path === '/monitors'" />
+						<GpuTable v-if="this.$route.path === '/gpu'" />
+						<MotherboardTable v-if="this.$route.path === '/motherboard'" />
+						<CaseTable v-if="this.$route.path === '/case'" />
+						<MemoryTable v-if="this.$route.path === '/memory'" />
+						<CaseFanTable v-if="this.$route.path === '/case_fan'" />
+					</zg-data>
+				</zing-grid>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
@@ -56,36 +56,36 @@ import MotherboardTable from "../components/Motherboard.vue";
 import PowerSupplyTable from "../components/Power_Supply.vue";
 import StorageTable from "../components/Storage.vue";
 export default {
-  name: "Products",
-  data() {
-    return {
-      dataUrl: "",
-    };
-  },
-  components: {
-    CaseFanTable,
-    CaseTable,
-    CpuTable,
-    GpuTable,
-    MemoryTable,
-    MonitorTable,
-    MotherboardTable,
-    PowerSupplyTable,
-    StorageTable,
-  },
-  created() {
-    if (this.$route.path) {
-      this.dataUrl = `http://3.95.218.141:3000${this.$route.path}`;
-    }
-    let token = localStorage.access_token;
-    this.access_token = `{"access_token": "${token}"}`;
-  },
-  watch: {
-    $route() {
-      this.dataUrl = "";
-      this.dataUrl = `http://3.95.218.141:3000${this.$route.path}`;
-    },
-  },
+	name: "Products",
+	data() {
+		return {
+			dataUrl: "",
+		};
+	},
+	components: {
+		CaseFanTable,
+		CaseTable,
+		CpuTable,
+		GpuTable,
+		MemoryTable,
+		MonitorTable,
+		MotherboardTable,
+		PowerSupplyTable,
+		StorageTable,
+	},
+	created() {
+		if (this.$route.path) {
+			this.dataUrl = `https://ayorakitkomputer.herokuapp.com${this.$route.path}`;
+		}
+		let token = localStorage.access_token;
+		this.access_token = `{"access_token": "${token}"}`;
+	},
+	watch: {
+		$route() {
+			this.dataUrl = "";
+			this.dataUrl = `https://ayorakitkomputer.herokuapp.com${this.$route.path}`;
+		},
+	},
 };
 </script>
 
